00120ac41f5933ade9968574e94f0fa1e73ba5d0
Josh Oynick <oynickj@gmail.com>
2013-11-22 09:45:30 -0500

    created folders

---
+++ silence_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SilenceApp::Application.config.secret_token = '29efe48a654e43de96e948b3c0a5b6d8f81850849e9dd1d601cd0b8736b98f2e4ced204e0ad2fd08ae6085de5f6b18004c96da937eb62ed3338964bb6e15acd2'
